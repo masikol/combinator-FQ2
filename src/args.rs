f85a09f702cce840fb87d7d5011f8f44716bb4b8
Maksim Sikolenko <maximdeynonih@gmail.com>
2026-06-28 20:01:39 +0300

    add args.rs

---
+++ src/args.rs
@@ -0,0 +1,134 @@
+
+use std::fs;
+use std::path::PathBuf;
+
+use clap::Parser;
+
+
+#[derive(Parser)]
+#[command(
+    name = "combinator_fq2",
+    version = "0.1.0",
+    about = "A program to find adjacent contigs by matching their ends of length k.",
+    long_about = None,
+    author = "Maksim Sikolenko",
+    help_template = "{name} {version}\nBy {author}\n\n{about}\n\nUSAGE:\n    {usage}\n\n{all-args}"
+)]
+struct RawArgs {
+
+    /// Input fasta file
+    #[arg(value_parser)]
+    input_fpath: String,
+
+    /// Minimum k to test.
+    /// Integer > 0; Default is 21 bp
+    #[arg(
+        short = 'i',
+        long,
+        default_value_t = 21,
+        value_parser 
+    )]
+    mink: usize,
+
+    /// Maximum k to test.
+    /// Integer > 0; Default is 127 bp
+    #[arg(
+        short = 'a',
+        long,
+        default_value_t = 127,
+        value_parser 
+    )]
+    maxk:usize,
+
+    /// Single k to test.
+    /// If speified, `-i` and `-a` options are ignored.
+    /// Integer > 0. Disabled by default
+    #[arg(
+        short = 'k',
+        long = "k-mer",
+        value_parser 
+    )]
+    k: Option<usize>,
+
+    /// Output directory.
+    /// Default value: `combinator-result`
+    #[arg(
+        short = 'o',
+        long = "outdir",
+        default_value_t = String::from("combinator-result"),
+        value_parser 
+    )]
+    outdir_path: String,
+}
+
+impl RawArgs {
+    fn validate(&self) -> Result<(), String> {
+
+        if self.mink == 0 {
+            return Err("Error: mink must not be zero".to_string());
+        }
+        if self.maxk == 0 {
+            return Err("Error: mink must not be zero".to_string());
+        }
+        if let Some(k) = self.k {
+            if k == 0 {
+                return Err("Error: k must not be zero".to_string());
+            }
+        }
+
+        if self.mink > self.maxk {
+            return Err(
+                format!(
+                    "Error: mink ({}) is greater than maxk ({}).",
+                    self.mink,
+                    self.maxk
+                )
+            );
+        }
+
+        if !PathBuf::from(&self.input_fpath).is_file() {
+            return Err(
+                format!(
+                    "Error: input file does not exist: `{}`",
+                    self.input_fpath
+                )
+            );
+        }
+
+        Ok(())
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Args {
+    pub mink: usize,
+    pub maxk: usize,
+    pub input_fpath: PathBuf,
+    pub outdir_path: PathBuf,
+}
+
+impl Args {
+
+    pub fn parse() -> Result<Args, String> {
+        let raw_args = RawArgs::parse();
+        if let Err(err_msg) = raw_args.validate() {
+            eprintln!("{}", err_msg);
+            return Err(err_msg);
+        }
+
+        let mut mink = raw_args.mink;
+        let mut maxk = raw_args.maxk;
+        if let Some(k) = raw_args.k {
+            mink = k;
+            maxk = k;
+        }
+
+        Ok(Args {
+            mink: mink,
+            maxk: maxk,
+            input_fpath: PathBuf::from(&raw_args.input_fpath),
+            outdir_path: PathBuf::from(&raw_args.outdir_path),
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 
+mod args;
 mod find_overlap;
 
+
+use std::fs;
+use std::process::ExitCode;
+
+use args::Args;
 use find_overlap::{find_overlap_s2s, find_overlap_e2s, find_overlap_e2e};
 
 
-fn main() {
+fn main() -> ExitCode {
+
+    let args = match Args::parse() {
+        Ok(args) => args,
+        Err(err_msg) => {
+            eprintln!("{}", err_msg);
+            return ExitCode::FAILURE;
+        }
+    };
+    println!("{:?}", args);
+
+    if create_outdir(&args).is_err() {
+        return ExitCode::FAILURE;
+    }
+
     let overlap: usize = find_overlap_s2s(
         &String::from("AGTCaaaaaaaaa"),
         &String::from("AGTCttttttttttttttt"),
@@ -28,4 +48,20 @@ fn main() {
         5
     );
     println!("overlap = {overlap}");
+
+    ExitCode::SUCCESS
+}
+
+fn create_outdir(args: &Args) -> Result<(), ()> {
+    if ! args.outdir_path.is_dir() {
+        if let Err(error) = fs::create_dir(&args.outdir_path) {
+            eprintln!(
+                "Error: cannot create directory `{}`",
+                args.outdir_path.display()
+            );
+            eprintln!("Reason: {}", error);
+            return Err(());
+        }
+    }
+    Ok(())
 }
